3c7a7108709477a7d663ac87a4cafa7fb08ae66f
Yash <yashlaxman@gmail.com>
2024-09-04 15:31:00 +0530

    add animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import Navbar from "./components/Navbar";
 import { motion, useScroll, useTransform } from "framer-motion";
-import Logo1 from "../src/assets/Synergia svg.svg";
 import Logo from "./components/Logo";
+import Logo1 from "../src/assets/test.svg";
 
 export default function App() {
-  const imgRef = useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: imgRef,
-    offset: ["start start", "end start"],
-  });
-
-  const imageOpacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
-  const navbarOpacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
-
   return (
     <div>
-      <Navbar navbarOpacity={navbarOpacity} />
+      <Navbar />
       <div className="h-screen relative flex justify-center items-center">
-        {/* <motion.img
-          src={Logo1}
-          ref={imgRef}
-          style={{ opacity: imageOpacity }}
-          className="w-96"
-        /> */}
         <Logo />
       </div>
       <div className="h-screen"></div>
